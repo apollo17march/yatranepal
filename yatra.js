9bdf6d9c4d5b79688fc2b87b4186fde5cc245ca2
apollo17march <apollo.vampire2@gmail.com>
2015-12-10 15:55:05 +0545

    moved 'fortune cookie' functionality into modules

--- yatra.js
+++ yatra.js
@@ -1,5 +1,6 @@
 var express= require('express');
 var app= express();
+var fortune = require('./lib/fortune.js');
 // set up handlebars view engine 
 var handlebars = require('express3-handlebars')        
 .create({ defaultLayout:'main' }); 
@@ -16,8 +17,7 @@ app.get('/', function(req, res){
 }); 
 
 app.get('/about', function(req, res){        
-	var randomFortune =fortunes[Math.floor(Math.random() * fortunes.length)];
- res.render('about', { fortune: randomFortune });
+ res.render('about', { fortune: fortune.getFortune() });
 
 });
 
@@ -34,13 +34,6 @@ app.use(function(err, req, res, next){
    res.render('500');
 });
 
-var fortunes = [
- "Conquer your fears or they will conquer you.",
- "Rivers need springs.",
- "Do not fear what you don't know.",
- "You will have a pleasant surprise.",
- "Whenever possible, keep it simple.",
-];
 
 
 app.listen(app.get('port'), function(){  
